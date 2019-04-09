ee165f2561edd99b9b2f0c373a9f7a709ec85a80
Aj Gill <ajgill@ucdavis.edu>
2019-04-09 15:00:28 -0700

    N-Repeated Elements
    In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
    Return the element repeated N times.

--- N-Repeated Elements/main.cpp
+++ N-Repeated Elements/main.cpp
@@ -7,9 +7,41 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <map>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int repeatedNTimes(vector<int>& A) {
+    int mostFreq = 0;
+    int largestCount = 0;
+    map<int,int> hMap;
+    for(int i=0;i< A.size(); i++){
+        //check if it exists, if it does, increment value
+        map<int,int>::iterator it;
+        it = hMap.find(A[i]);
+        if(it != hMap.end()){
+            it->second = it->second + 1;
+        }
+        //if it doesnt, then insert with the value set to 1
+        else{
+            hMap.insert(make_pair(A[i], 1));
+        }
+    }
+    for(map<int,int>::iterator iter = hMap.begin(); iter != hMap.end(); ++iter){
+        //cout << "Number:" << iter->first << " Frequency:" << iter->second << "\n";
+        if(iter->second > largestCount){
+            largestCount = iter->second;
+            mostFreq = iter->first;
+        }
+    }
+    return mostFreq;
 }
+
+int main() {
+    vector<int> candy = {8, 3, 2, 3};
+    int answer = repeatedNTimes(candy);
+    cout << "answer is " << answer << "\n";
+}
+
+
